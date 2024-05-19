a5394d75d1aab21ed9a86aec68385bdb5f429aea
Matthew Berryman <matthew@acrossthecloud.net>
2024-05-19 12:30:43 +0930

    change clone path in CI script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,6 +6,7 @@
 #  Created by Matthew Berryman on 19/5/2024.
 #  
 
+cd /Volumes/workspace || exit
 git clone https://github.com/matthewberryman/simple-admiral-blocker.git
-echo $PWD
+echo "$PWD"
 ls -al
