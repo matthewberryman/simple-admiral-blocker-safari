957063fc6952e2a81bb9e4ffeba5c21bab07ed1c
Matthew Berryman <matthew@acrossthecloud.net>
2024-05-19 12:19:46 +0930

    add post-clone script

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Simple Admiral Blocker
+#
+#  Created by Matthew Berryman on 19/5/2024.
+#  
+
+git clone https://github.com/matthewberryman/simple-admiral-blocker.git
